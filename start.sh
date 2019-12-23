ae8621c4ca26876ad86ce35b87d35db5e260c0b8
殷豪 <jjyyg1123@gmail.com>
2019-12-23 23:49:45 +0800

    feat: setting up for vault dev and mongo role

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export $(grep -v '^#' .env | xargs)
+export VAULT_ADDR=http://localhost:${VAULT_PORT}
+
+docker-compose up -d
+echo ${VAULT_TOKEN} | vault login -
+
+vault secrets enable database
+
+vault write database/config/my-mongodb-database \
+  plugin_name=mongodb-database-plugin \
+  allowed_roles="my-role" \
+  connection_url="mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/admin" 
+
+vault write database/roles/my-role \
+  db_name=my-mongodb-database \
+  creation_statements='{ "db": "admin", "roles": [{"role": "readWrite", "db": "'"${MONGO_DATABASE}"'"}]}' \
+  default_ttl="1h" \
+  max_ttl="24h"
